d752e09239d75a4b0df1884cc94e0cd3763f27c1
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-15 18:26:33 -0500

    chore: add teams and roles

--- terraform/gh-org.tf
+++ terraform/gh-org.tf
@@ -2,4 +2,18 @@ resource "github_organization_settings" "dffp" {
   name          = "DFFP"
   billing_email = data.aws_ssm_parameter.gh_org_email.value
   location      = "United States of America"
+
+  default_repository_permission = "write"
+
+  members_can_create_repositories         = false
+  members_can_create_public_repositories  = false
+  members_can_create_private_repositories = false
+  members_can_fork_private_repositories   = false
+
+  advanced_security_enabled_for_new_repositories               = true
+  secret_scanning_enabled_for_new_repositories                 = true
+  secret_scanning_push_protection_enabled_for_new_repositories = true
+  dependabot_security_updates_enabled_for_new_repositories     = true
+  dependency_graph_enabled_for_new_repositories                = true
+  dependabot_alerts_enabled_for_new_repositories               = true
 }
--- terraform/gh-users.tf
+++ terraform/gh-users.tf
@@ -1,15 +1,27 @@
 variable "users" {
   type = map(list(string))
   default = {
-    ryanemcdaniel     = ["admin"]
-    "Khatry-With-A-Y" = ["member"]
-    "5hubham6"        = ["member"]
+    ryanemcdaniel     = ["admin", "Admin", "maintainer"]
+    "Khatry-With-A-Y" = ["member", "Dev", "member"]
+    "5hubham6"        = ["member", "Dev", "member"]
   }
 }
 
 data "github_user" "users" {
   for_each = var.users
   username = each.key
+}
+
+locals {
+  main_force_push_bypassers = [
+    data.github_user.users["ryanemcdaniel"].node_id
+  ]
+  main_dismissal_restrictions = [
+    "/${data.github_user.users["ryanemcdaniel"].username}"
+  ]
+  main_pull_request_bypassers = [
+    "/${data.github_user.users["ryanemcdaniel"].username}"
+  ]
 }
 
 resource "github_membership" "users" {
